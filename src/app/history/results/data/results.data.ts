9818f7e9b8a9be628c744f755b36cdf6517d6bc4
Nick Prozorovsky <nick.prozorovsky@vodori.com>
2017-05-13 22:10:41 -0500

    all pages complete

--- src/app/history/results/data/results.data.ts
+++ src/app/history/results/data/results.data.ts
@@ -2,6 +2,14 @@ import { Result } from "../model/result.model";
 
 export const RESULTS_DATA: Result[] = [
     {
+        year: 2017,
+        sectionals: '2nd',
+        regionals: '6th',
+        nationals: '-',
+        teamPageLabel: 'USAU',
+        teamPageLink: 'http://play.usaultimate.org/events/teams/?EventTeamId=TIUxXrZ4pDNhNi4M3QA1J%2b9D8kAdNss8Ny4JyUUuZhg%3d'
+    },
+    {
         year: 2016,
         sectionals: '3rd',
         regionals: '3rd',
@@ -16,5 +24,93 @@ export const RESULTS_DATA: Result[] = [
         nationals: '13th',
         teamPageLabel: 'USAU',
         teamPageLink: 'http://play.usaultimate.org/teams/events/Eventteam/?TeamId=T%2bV6D2XyLTlkLA%2fX2WpaiPGnYy6S3Dn%2fmcblfo08DNo%3d'
+    },
+    {
+        year: 2014,
+        sectionals: '1st',
+        regionals: '5th',
+        nationals: '-',
+        teamPageLabel: 'USAU',
+        teamPageLink: 'http://play.usaultimate.org/teams/events/Eventteam/?TeamId=1z8hrygGYaDwJqk8F%2fmP4lOk9BD3%2fI%2bKW33IHo8qF9E%3d'
+    },
+    {
+        year: 2013,
+        sectionals: '1st',
+        regionals: '1st',
+        nationals: '17th',
+        teamPageLabel: 'USAU',
+        teamPageLink: 'http://scores.usaultimate.org/scores/#college-open/team/1794'
+    },
+    {
+        year: 2012,
+        sectionals: '1st',
+        regionals: '3rd',
+        nationals: '-',
+        teamPageLabel: 'USAU',
+        teamPageLink: 'http://scores.usaultimate.org/scores2012/#college-open/team/1794'
+    },
+    {
+        year: 2011,
+        sectionals: '1st',
+        regionals: '1st',
+        nationals: '17th',
+        teamPageLabel: 'USAU',
+        teamPageLink: 'http://scores.usaultimate.org/scores2011/#college-open/team/1794'
+    },
+    {
+        year: 2010,
+        sectionals: '1st',
+        regionals: '1st',
+        nationals: '17th',
+        teamPageLabel: 'USAU',
+        teamPageLink: 'http://scores.usaultimate.org/scores2010/#college-open/team/1794'
+    },
+    {
+        year: 2009,
+        sectionals: '2nd',
+        regionals: '1st',
+        nationals: '13th',
+        teamPageLabel: 'USAU',
+        teamPageLink: 'http://ultimate.scorereport.net/2009/scores.cgi?div=18&page=3&team=1794'
+    },
+    {
+        year: 2008,
+        sectionals: '1st',
+        regionals: '2nd',
+        nationals: '5th',
+        teamPageLabel: 'USAU',
+        teamPageLink: 'http://ultimate.scorereport.net/2008/scores.cgi?div=18&page=3&team=1794'
+    },
+    {
+        year: 2007,
+        sectionals: '2st',
+        regionals: '4th',
+        nationals: '-',
+        teamPageLabel: 'USAU',
+        teamPageLink: 'http://ultimate.scorereport.net/2007/scores.cgi?div=18&page=3&team=1794'
+    },
+    {
+        year: 2006,
+        sectionals: '3rd',
+        regionals: '7th',
+        nationals: '-',
+        teamPageLabel: 'USAU',
+        teamPageLink: 'http://ultimate.scorereport.net/2006/scores.cgi?div=18&page=3&team=1794'
+    },
+    {
+        year: 2005,
+        sectionals: '2st',
+        regionals: '3rd',
+        nationals: '-',
+        teamPageLabel: 'USAU',
+        teamPageLink: 'http://ultimate.scorereport.net/2005/scores.cgi?div=18&page=3&team=1794'
+    },
+    {
+        year: 2004,
+        sectionals: '1st',
+        regionals: '2nd',
+        nationals: '17th',
+        teamPageLabel: 'USAU',
+        teamPageLink: 'http://ultimate.scorereport.net/2004/scores.cgi?div=18&page=3&team=1794'
     }
 ];
--- src/app/team/roster/data/rosters.data.ts
+++ src/app/team/roster/data/rosters.data.ts
@@ -9,6 +9,96 @@ export const ROSTER_DATA: RosterYear[] = [
                 number: 2,
                 name: `Ben 'Beef' Sabourin`,
                 year: 'Sophomore'
+            },
+            {
+                number: 3,
+                name: 'Cody Gray',
+                year: 'Grad'
+            },
+            {
+                number: 6,
+                name: 'Jacob Cuthbert',
+                year: 'Sophomore'
+            },
+            {
+                number: 7,
+                name: 'Kevin Lin',
+                year: 'Junior'
+            },
+            {
+                number: 8,
+                name: 'Devin Kelly',
+                year: 'Sophomore'
+            },
+            {
+                number: 13,
+                name: 'Walter Jaskowiak',
+                year: 'Sophomore'
+            },
+            {
+                number: 18,
+                name: 'Hector Moreno',
+                year: 'Junior'
+            },
+            {
+                number: 19,
+                name: 'Jacob Komenda*',
+                year: 'Junior'
+            },
+            {
+                number: 23,
+                name: 'Jarred Elliott',
+                year: 'Senior'
+            },
+            {
+                number: 24,
+                name: 'Josh Deeman',
+                year: 'Sophomore'
+            },
+            {
+                number: 28,
+                name: 'Frank Dore',
+                year: 'Sophomore'
+            },
+            {
+                number: 29,
+                name: 'Ben Foster*',
+                year: 'Senior'
+            },
+            {
+                number: 33,
+                name: 'Jacob Trueb',
+                year: 'Junior'
+            },
+            {
+                number: 35,
+                name: 'Cameron Hartman',
+                year: 'Sophomore'
+            },
+            {
+                number: 38,
+                name: 'John Kulikowski',
+                year: 'Sophomore'
+            },
+            {
+                number: 47,
+                name: 'Grant Whitman',
+                year: 'Junior'
+            },
+            {
+                number: 81,
+                name: `Chris 'Books' Buchta*`,
+                year: 'Senior'
+            },
+            {
+                number: 95,
+                name: 'Tat Shing',
+                year: 'Junior'
+            },
+            {
+                number: undefined,
+                name: 'Erik Huemiller',
+                year: 'Coach'
             }
         ]
     },
@@ -20,6 +110,136 @@ export const ROSTER_DATA: RosterYear[] = [
                 number: 1,
                 name: 'Nick Prozorovsky*',
                 year: 'Senior'
+            },
+            {
+                number: 2,
+                name: 'Ben Sabourin',
+                year: 'Freshman'
+            },
+            {
+                number: 3,
+                name: 'Cody Gray',
+                year: 'Grad'
+            },
+            {
+                number: 4,
+                name: 'Michael Cosmos',
+                year: 'Senior'
+            },
+            {
+                number: 7,
+                name: 'Kevin Lin',
+                year: 'Sophomore'
+            },
+            {
+                number: 8,
+                name: 'Johnny Saniat',
+                year: 'Senior'
+            },
+            {
+                number: 10,
+                name: 'Jacob Komenda',
+                year: 'Sophomore'
+            },
+            {
+                number: 12,
+                name: 'Kan Zi Yang',
+                year: 'Senior'
+            },
+            {
+                number: 13,
+                name: 'Mark Peterson',
+                year: 'Senior'
+            },
+            {
+                number: 17,
+                name: 'Prayag Patel*',
+                year: 'Senior'
+            },
+            {
+                number: 18,
+                name: 'Alex Guy',
+                year: 'Senior'
+            },
+            {
+                number: 23,
+                name: 'Jarred Elliott',
+                year: 'Junior'
+            },
+            {
+                number: 24,
+                name: 'Josh Deeman',
+                year: 'Freshman'
+            },
+            {
+                number: 27,
+                name: 'Wilson Wang',
+                year: 'Senior'
+            },
+            {
+                number: 29,
+                name: 'Ben Foster',
+                year: 'Junior'
+            },
+            {
+                number: 37,
+                name: 'Walter Jaskowiak',
+                year: 'Freshman'
+            },
+            {
+                number: 38,
+                name: 'Jacob Trueb',
+                year: 'Sophomore'
+            },
+            {
+                number: 41,
+                name: 'Devin Kelly',
+                year: 'Freshman'
+            },
+            {
+                number: 44,
+                name: 'Ryan Kantor',
+                year: 'Freshman'
+            },
+            {
+                number: 48,
+                name: 'Hector Moreno',
+                year: 'Sophomore'
+            },
+            {
+                number: 71,
+                name: 'Bobby Gazdziak',
+                year: 'Senior'
+            },
+            {
+                number: 81,
+                name: 'Chris Buchta*',
+                year: 'Junior'
+            },
+            {
+                number: 88,
+                name: 'Tim Chu',
+                year: 'Senior'
+            },
+            {
+                number: 95,
+                name: 'Tat Shing',
+                year: 'Sophomore'
+            },
+            {
+                number: 97,
+                name: 'Jacob Cuthbert',
+                year: 'Freshman'
+            },
+            {
+                number: undefined,
+                name: 'Matt West',
+                year: 'Assistant Coach'
+            },
+            {
+                number: undefined,
+                name: 'Erik Huemiller',
+                year: 'Coach'
             }
         ]
     },
@@ -28,9 +248,512 @@ export const ROSTER_DATA: RosterYear[] = [
         imagePath: './assets/team-photo-2015.jpg',
         players: [
             {
+                number: 0,
+                name: 'Max Willis',
+                year: 'Senior'
+            },
+            {
                 number: 1,
                 name: 'Nick Prozorovsky*',
                 year: 'Junior'
+            },
+            {
+                number: 4,
+                name: 'Michael Pohling',
+                year: 'Senior'
+            },
+            {
+                number: 7,
+                name: 'Matt West',
+                year: 'Grad'
+            },
+            {
+                number: 8,
+                name: 'John Saniat*',
+                year: 'Junior'
+            },
+            {
+                number: 10,
+                name: 'Joe Moyles',
+                year: 'Senior'
+            },
+            {
+                number: 11,
+                name: 'Jon Mckoy',
+                year: 'Senior'
+            },
+            {
+                number: 12,
+                name: 'Kan Zi Yang',
+                year: 'Junior'
+            },
+            {
+                number: 13,
+                name: 'Mark Peterson',
+                year: 'Junior'
+            },
+            {
+                number: 17,
+                name: 'Prayag Patel',
+                year: 'Junior'
+            },
+            {
+                number: 18,
+                name: 'Eric Bultman',
+                year: 'Senior'
+            },
+            {
+                number: 21,
+                name: 'Jarred Elliott',
+                year: 'Sophomore'
+            },
+            {
+                number: 23,
+                name: 'Tyler DeNooyer',
+                year: 'Grad'
+            },
+            {
+                number: 27,
+                name: 'Wilson Wang',
+                year: 'Junior'
+            },
+            {
+                number: 29,
+                name: 'Ben Foster',
+                year: 'Sophomore'
+            },
+            {
+                number: 35,
+                name: 'Marty Markenson*',
+                year: 'Senior'
+            },
+            {
+                number: 38,
+                name: 'Grant Hallan',
+                year: 'Senior'
+            },
+            {
+                number: 46,
+                name: 'Kevin Birchok',
+                year: 'Senior'
+            },
+            {
+                number: 69,
+                name: 'Jacob Komenda',
+                year: 'Freshman'
+            },
+            {
+                number: 75,
+                name: 'Eric Badger',
+                year: 'Grad'
+            },
+            {
+                number: 81,
+                name: 'Chris Buchta',
+                year: 'Sophomore'
+            },
+            {
+                number: 97,
+                name: 'Kevin Lin',
+                year: 'Freshman'
+            },
+            {
+                number: undefined,
+                name: 'Matt Stupca',
+                year: 'Coach'
+            }
+        ]
+    },
+    {
+        year: 2014,
+        imagePath: './assets/team-photo-2014.jpg',
+        players: [
+            {
+                number: 0,
+                name: 'Max Willis',
+                year: 'Junior'
+            },
+            {
+                number: 1,
+                name: 'Nick Prozorovsky',
+                year: 'Sophomore'
+            },
+            {
+                number: 3,
+                name: 'Sean Hill',
+                year: 'Senior'
+            },
+            {
+                number: 4,
+                name: 'Michael Pohling',
+                year: 'Junior'
+            },
+            {
+                number: 6,
+                name: 'Nick Oo',
+                year: 'Junior'
+            },
+            {
+                number: 8,
+                name: 'Johnny Saniat',
+                year: 'Sophomore'
+            },
+            {
+                number: 10,
+                name: 'Joe Moyles',
+                year: 'Junior'
+            },
+            {
+                number: 11,
+                name: 'Jon Mckoy',
+                year: 'Junior'
+            },
+            {
+                number: 12,
+                name: 'Kan Zi Yang',
+                year: 'Sophomore'
+            },
+            {
+                number: 17,
+                name: 'Prayag Patel',
+                year: 'Sophomore'
+            },
+            {
+                number: 18,
+                name: 'Eric Bultman*',
+                year: 'Junior'
+            },
+            {
+                number: 21,
+                name: 'Chris Buchta',
+                year: 'Freshman'
+            },
+            {
+                number: 22,
+                name: 'Brian Rathunde',
+                year: 'Senior'
+            },
+            {
+                number: 23,
+                name: 'Greg Hill',
+                year: 'Senior'
+            },
+            {
+                number: 24,
+                name: 'Jim Riddle',
+                year: 'Senior'
+            },
+            {
+                number: 27,
+                name: 'Wilson Wang',
+                year: 'Sophomore'
+            },
+            {
+                number: 30,
+                name: 'Steve Brand',
+                year: 'Grad'
+            },
+            {
+                number: 35,
+                name: 'Marty Markenson*',
+                year: 'Junior'
+            },
+            {
+                number: 38,
+                name: 'Grant Hallan',
+                year: 'Junior'
+            },
+            {
+                number: 39,
+                name: 'Ross Tuttle',
+                year: 'Senior'
+            },
+            {
+                number: 44,
+                name: 'Michael Cosmos',
+                year: 'Sophomore'
+            },
+            {
+                number: 46,
+                name: 'Kevin Birchok',
+                year: 'Junior'
+            },
+            {
+                number: 64,
+                name: 'Neal Phelps*',
+                year: 'Grad'
+            },
+            {
+                number: 69,
+                name: 'Tyler DeNooyer',
+                year: 'Grad'
+            },
+            {
+                number: 81,
+                name: 'Arthur Ding',
+                year: 'Sophomore'
+            },
+            {
+                number: undefined,
+                name: 'Walden Nelson',
+                year: 'Coach'
+            }
+        ]
+    },
+    {
+        year: 2013,
+        imagePath: './assets/team-photo-2013.jpg',
+        players: [
+            {
+                number: 1,
+                name: 'Nick Prozorovsky',
+                year: 'Freshman'
+            },
+            {
+                number: 3,
+                name: 'Sean Hill',
+                year: 'Junior'
+            },
+            {
+                number: 4,
+                name: 'Rob Dreier',
+                year: 'Senior'
+            },
+            {
+                number: 6,
+                name: 'Brad Bolliger',
+                year: 'Senior'
+            },
+            {
+                number: 9,
+                name: 'Preston Goulson',
+                year: 'Senior'
+            },
+            {
+                number: 11,
+                name: 'Jon Mckoy',
+                year: 'Sophomore'
+            },
+            {
+                number: 12,
+                name: 'Derek Yan',
+                year: 'Freshman'
+            },
+            {
+                number: 18,
+                name: 'Eric Bultman',
+                year: 'Sophomore'
+            },
+            {
+                number: 20,
+                name: 'Tyler Anderson',
+                year: 'Senior'
+            },
+            {
+                number: 22,
+                name: 'Brian Rathunde',
+                year: 'Junior'
+            },
+            {
+                number: 23,
+                name: 'Ryan Smith*',
+                year: 'Grad'
+            },
+            {
+                number: 27,
+                name: 'Wilson Wang',
+                year: 'Freshman'
+            },
+            {
+                number: 30,
+                name: 'Colin Reid*',
+                year: 'Senior'
+            },
+            {
+                number: 34,
+                name: 'Michael Pohling',
+                year: 'Sophomore'
+            },
+            {
+                number: 35,
+                name: 'Marty Markenson',
+                year: 'Sophomore'
+            },
+            {
+                number: 39,
+                name: 'Ross Tuttle',
+                year: 'Junior'
+            },
+            {
+                number: 44,
+                name: 'Nate Gibbons*',
+                year: 'Senior'
+            },
+            {
+                number: 45,
+                name: 'Greg Hill',
+                year: 'Junior'
+            },
+            {
+                number: 64,
+                name: 'Neal Phelps*',
+                year: 'Grad'
+            },
+            {
+                number: 65,
+                name: 'Michael Cosmos',
+                year: 'Freshman'
+            },
+            {
+                number: 87,
+                name: 'Adam Wright',
+                year: 'Senior'
+            },
+            {
+                number: 99,
+                name: 'Johnny Saniat',
+                year: 'Junior'
+            },
+            {
+                number: undefined,
+                name: 'Walden Nelson',
+                year: 'Coach'
+            }
+        ]
+    },
+    {
+        year: 2011,
+        imagePath: './assets/team-photo-2011.jpg',
+        players: [
+            {
+                number: 2,
+                name: 'Joe Sidrys',
+                year: 'Senior'
+            },
+            {
+                number: 5,
+                name: 'Jack Krieger',
+                year: 'Junior'
+            },
+            {
+                number: 8,
+                name: 'Kyle McKinley',
+                year: 'Grad'
+            },
+            {
+                number: 13,
+                name: 'Tom Rudwick',
+                year: 'Senior'
+            },
+            {
+                number: 14,
+                name: 'Austin Lien',
+                year: 'Senior'
+            },
+            {
+                number: 15,
+                name: 'Alex Komisarz',
+                year: 'Senior'
+            },
+            {
+                number: 16,
+                name: 'Andy Kilinskis',
+                year: 'Junior'
+            },
+            {
+                number: 18,
+                name: 'Kurt Zoellick',
+                year: 'Grad'
+            },
+            {
+                number: 22,
+                name: 'Brian Chrysler',
+                year: 'Grad'
+            },
+            {
+                number: 23,
+                name: 'Ryan Smith',
+                year: 'Grad'
+            },
+            {
+                number: 25,
+                name: 'Mark Hirschman',
+                year: 'Senior'
+            },
+            {
+                number: 27,
+                name: 'Chris Hidaka',
+                year: 'Grad'
+            },
+            {
+                number: 29,
+                name: 'Matt Kurley',
+                year: 'Grad'
+            },
+            {
+                number: 32,
+                name: 'Chris Kvistad',
+                year: 'Junior'
+            },
+            {
+                number: 33,
+                name: 'Don McDonald',
+                year: 'Grad'
+            },
+            {
+                number: 36,
+                name: 'Brian Pierce',
+                year: 'Junior'
+            },
+            {
+                number: 47,
+                name: 'Jon Hatcher',
+                year: 'Senior'
+            },
+            {
+                number: 61,
+                name: 'Brian Phelan',
+                year: 'Grad'
+            },
+            {
+                number: 64,
+                name: 'Neal Phelps*',
+                year: 'Senior'
+            },
+            {
+                number: 80,
+                name: 'Jon Ogrodnik',
+                year: 'Grad'
+            },
+            {
+                number: 81,
+                name: 'Suleyman Ahmad',
+                year: 'Senior'
+            },
+            {
+                number: 84,
+                name: `Charlie O'Brien`,
+                year: 'Grad'
+            },
+            {
+                number: 87,
+                name: 'Adam Wright',
+                year: 'Junior'
+            },
+            {
+                number: 88,
+                name: 'Kevin Bruns',
+                year: 'Junior'
+            },
+            {
+                number: 90,
+                name: 'Zachary Frantz',
+                year: 'Junior'
+            },
+            {
+                number: undefined,
+                name: 'Walden Nelson',
+                year: 'Coach'
             }
         ]
     }
